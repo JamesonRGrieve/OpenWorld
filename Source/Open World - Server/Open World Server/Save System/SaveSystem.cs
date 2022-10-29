011842974371aba0ed162715bc30fa7160e97dbd
DarkIrata <git@ipmix.de>
2022-10-29 19:23:34 +0200

    Fixed merge errors

--- Source/Open World - Server/Open World Server/Save System/SaveSystem.cs
+++ Source/Open World - Server/Open World Server/Save System/SaveSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using OpenWorldServer.Services;
 
 namespace OpenWorldServer
 {
@@ -8,7 +9,7 @@ namespace OpenWorldServer
     {
         public static void SaveBannedIPs(Dictionary<string, string> IPs)
         {
-            string folderPath = Server.mainFolderPath;
+            string folderPath = PathProvider.MainFolderPath;
             string filepath = folderPath + Path.DirectorySeparatorChar + "bans_ip.dat";
 
             BinaryFormatter formatter = new BinaryFormatter();
@@ -24,7 +25,7 @@ namespace OpenWorldServer
 
         public static BanDataHolder LoadBannedIPs()
         {
-            string path = Server.mainFolderPath + Path.DirectorySeparatorChar + "bans_ip.dat";
+            string path = PathProvider.MainFolderPath + Path.DirectorySeparatorChar + "bans_ip.dat";
 
             BinaryFormatter formatter = new BinaryFormatter();
 
